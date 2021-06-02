807803095293823629159b5f4548032ac7b4a5dd
johanneskasser <johannes.kasser@stud.fh-campuswien.ac.at>
2021-06-02 11:42:36 +0200

    added CheckPassword Method

--- src/main/java/at/ac/fhcampuswien/Password.java
+++ src/main/java/at/ac/fhcampuswien/Password.java
@@ -3,6 +3,89 @@ package at.ac.fhcampuswien;
 public class Password {
 
     public static boolean checkPassword(String pw) {
+        boolean length = checkLength(pw);
+        boolean capitals = checkCapitals(pw);
+        boolean digits = checkDigits(pw);
+        boolean special = checkSpecialCharacters(pw);
+        boolean increase = checkIncrease(pw);
+        boolean continuous = checkContinuous(pw);
+        return length && capitals && digits && special && increase && continuous;
+    }
+
+    public static boolean checkLength(String pw){
+        //Checks if pw is the right length
+        return pw.length() >= 8 && pw.length() <= 25;
+    }
+
+    public static boolean checkCapitals(String pw){
+        //Checks if pw contains Upper Case and Lower Case digits
+        char[] pwArray = pw.toCharArray();
+        int up = 0, down = 0;
+        for (char c : pwArray) {
+            if (Character.isUpperCase(c)) {
+                up++;
+            } else if (Character.isLowerCase(c)){
+                down++;
+            }
+        }
+        return up > 0 && down > 0;
+    }
+
+    public static boolean checkDigits(String pw){
+        //Checks if pw contains digits
+        char[] chars = pw.toCharArray();
+        for(char c : chars){
+            if(Character.isDigit(c)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkSpecialCharacters(String pw){
+        //Checks if pw contains special characters
+        final String VALID_SPECIAL = "[()#$?!%/@]";
+        char[] chars = pw.toCharArray();
+        for(char c : chars){
+            if(Character.toString(c).matches(VALID_SPECIAL)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkIncrease(String pw){
+        //Check if pw contains increasing numbers
+        char[] chars = pw.toCharArray();
+        for (int i = 0; i < pw.length() - 2; i++) {
+            if(Character.isDigit(chars[i])) {
+                if(chars[i] == chars[i+1] - 1 && chars[i] == chars[i+2] - 2){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkContinuous(String pw){
+        //Checks if pw contains continuous numbers
+        int cont = 0;
+        char[] chars = pw.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if(Character.isDigit(chars[i])){
+                for (int j = i + 1; j < chars.length; j++) {
+                    if(Character.isDigit(chars[j])){
+                        if(cont < 3){
+                            if(Character.getNumericValue(chars[i]) == Character.getNumericValue(chars[j])){
+                                cont++;
+                            }
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
         return true;
     }
 }
--- src/test/java/at/ac/fhcampuswien/PasswordTest.java
+++ src/test/java/at/ac/fhcampuswien/PasswordTest.java
@@ -20,85 +20,99 @@ public class PasswordTest {
 
     @Test
     public void passwordCheck_length(){
-        boolean actual = Password.checkPassword("hello");
+        boolean actual = Password.checkLength("hello");
         assertFalse(actual);
     }
 
+
+    //
     @Test
     public void passwordCheck_length2(){
-        boolean actual = Password.checkPassword("hello1234mynameisjohanneskasser");
+        boolean actual = Password.checkLength("hello1234mynameisjohanneskasser");
         assertFalse(actual);
     }
 
     @Test
     public void passwordCheck_length3(){
-        boolean actual = Password.checkPassword("hellowhatsup");
+        boolean actual = Password.checkLength("helloWhatsup1!");
         assertTrue(actual);
     }
 
     @Test
     public void passwordCheck_capitals(){
-        boolean actual = Password.checkPassword("HelLo");
+        boolean actual = Password.checkCapitals("Hello");
         assertTrue(actual);
     }
 
     @Test
     public void passwordCheck_capitals2(){
-        boolean actual = Password.checkPassword("hello");
+        boolean actual = Password.checkCapitals("hello");
         assertFalse(actual);
     }
 
     @Test
     public void passwordCheck_capitals3(){
-        boolean actual = Password.checkPassword("HELLO");
+        boolean actual = Password.checkCapitals("HELLO");
         assertFalse(actual);
     }
 
     @Test
     public void passwordCheck_numbers(){
-        boolean actual = Password.checkPassword("supersecure132");
+        boolean actual = Password.checkDigits("superSecure132!");
         assertTrue(actual);
     }
 
     @Test
     public void passwordCheck_numbers2(){
-        boolean actual = Password.checkPassword("supersecure");
+        boolean actual = Password.checkDigits("supersecure");
         assertFalse(actual);
     }
 
     @Test
     public void passwordCheck_special(){
-        boolean actual = Password.checkPassword("supersecure132A1!");
+        boolean actual = Password.checkSpecialCharacters("supersecure132A1!");
         assertTrue(actual);
     }
 
     @Test
     public void passwordCheck_special2(){
-        boolean actual = Password.checkPassword("supersecure");
+        boolean actual = Password.checkSpecialCharacters("supersecure");
         assertFalse(actual);
     }
 
     @Test
     public void passwordCheck_ongoingnumbers(){
-        boolean actual = Password.checkPassword("supersecure132");
+        boolean actual = Password.checkIncrease("supersecure132");
         assertTrue(actual);
     }
 
     @Test
     public void passwordCheck_ongoingnumbers2(){
-        boolean actual = Password.checkPassword("supersecure123");
+        boolean actual = Password.checkIncrease("supersecure123");
         assertFalse(actual);
     }
 
     @Test
     public void passwordCheck_threetimes(){
-        boolean actual = Password.checkPassword("supersecure132");
+        boolean actual = Password.checkContinuous("supersecure132");
         assertTrue(actual);
     }
 
     @Test
     public void passwordCheck_threetimes2(){
-        boolean actual = Password.checkPassword("supersecure99999999");
+        boolean actual = Password.checkContinuous("supersecure99999999");
+        assertFalse(actual);
+    }
+
+    @Test
+    public void passwordCheck_ValidPW(){
+        boolean actual = Password.checkPassword("suPerSecure931!");
+        assertTrue(actual);
+    }
+
+    @Test
+    public void passwordCheck_InvalidPW(){
+        boolean actual = Password.checkPassword("wackPassword");
         assertFalse(actual);
     }
 }
